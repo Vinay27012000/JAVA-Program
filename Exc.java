89b419cbdb6dfd2f0f933597e9eb72e96a3745c8
VINAY GUPTA <95950864+Vinay27012000@users.noreply.github.com>
2022-05-20 13:57:26 +0530

    Add files via upload

---
+++ Exc.java
@@ -0,0 +1,34 @@
+package javaprogram;
+
+import java.util.Scanner;
+
+public class Exc {
+	 int a,b;
+	 Exc(){
+		 a=b=0;
+	 }
+	 Exc (int x,int y){
+		a=x;b=y;
+	 }
+	 void func() {
+		 try {
+			 int c=a/b;
+			 System.out.println("no exception");
+		 }
+		 catch(ArithmeticException e) 
+		 {
+			 System.out.println(e.toString());
+		 }
+	 }
+		public static void main(String[] args) {
+			int a,b;
+			Scanner s=new Scanner(System.in);
+			System.out.println("enter two numbers :");
+			a=s.nextInt();
+			b=s.nextInt();
+			Exc obj=new Exc(a,b);
+			obj.func();
+
+		}
+
+	}
